15d0369ff0c5013a0b6c8e5f6692dd32f7d688af
Jamal Derrick <jamalderrick6@gmail.com>
2020-12-15 11:29:51 +0300

    [recursive-pagination] load api paginated data recursively

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'https://5b5cb0546a725000148a67ab.mockapi.io/api/v1/users'
+
+class App extends  React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      rowData : [],
+      page: 1,
+      limit: 20
+    }
+  }
+
+  componentDidMount() {
+    let limit = 20;
+    const getUsers = async function(page = 1) {
+      var apiResults = await fetch(url + `?page=${page}&limit=${limit}`)
+      .then(res=>{
+        return res.json();
+        });
+        
+        return apiResults;
+    }
+
+      const getEntireUserList = async function(page = 1) {
+        const results = await getUsers(page);
+        console.log("Retreiving data from API for page : " + page);
+        if (results.length>0) {
+          return results.concat(await getEntireUserList(page+1));
+        } else {
+          return results;
+        }
+      };
+
+      (async ()=>{
+
+        const entireList=await getEntireUserList();
+        console.log(entireList);
+    })();
+  }
+
+
+
+  render(){
+    return(
+      <>
+        This is an app
+      </>
+
+    )
+  }
 }
 
 export default App;
